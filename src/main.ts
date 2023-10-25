b12b2b4226491ed6baedf6e7f7f926f298e9f057
ormunoz <or.munozf@gmail.com>
2023-10-25 19:05:55 -0300

    se descargan los paquetes necesarios para trabajar

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,33 @@
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import { createPinia } from "pinia";
+import Popper from "vue3-popper";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas , faUser, faEnvelope, faUserSecret } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faTwitter } from '@fortawesome/free-brands-svg-icons'
 
-createApp(App).use(router).mount('#app')
+
+const pinia = createPinia();
+const app = createApp(App);
+library.add(faUser, faEnvelope, faUserSecret, faTwitter, fas);
+app.directive("click-outside", {
+  mounted(el, binding) {
+    el.clickOutsideEvent = function (event: Event) {
+      if (!(el === event.target || el.contains(event.target))) {
+        binding.value(event, el);
+      }
+    };
+    document.body.addEventListener("mousedown", el.clickOutsideEvent);
+  },
+  unmounted(el) {
+    document.body.removeEventListener("mousedown", el.clickOutsideEvent);
+  },
+});
+app.use(pinia);
+app.use(router);
+app.component("Popper", Popper);
+app.component('font-awesome-icon', FontAwesomeIcon);
+app.mount("#app");
