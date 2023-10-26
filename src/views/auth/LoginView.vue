5510625055ebccee14477998b6d5b76001c5b5e3
ormunoz <or.munozf@gmail.com>
2023-10-25 23:39:13 -0300

    se modifican colores de login

--- src/views/auth/LoginView.vue
+++ src/views/auth/LoginView.vue
@@ -7,11 +7,10 @@
             <section class="container">
                 <article class="row">
                     <div class="col-md-6 offset-md-3">
-                        <img class="mb-4" src="/img/logo.png" width="200">
                         <h1 class="h3 mb-3 fw-normal">Inicio de sesión</h1>
-                        <TextField v-model="userLogin.userName" type="text" label="Nombre" placeholder="Nombre completo" />
+                        <TextField v-model="userLogin.rut" type="text" label="Nombre" placeholder="Nombre completo" />
                         <TextField v-model="userLogin.password" type="password" label="Contraseña" placeholder="Password" />
-                        <button class="w-100 btn btn-lg btn-primary mt-4" @click="login">Iniciar Sesión</button>
+                        <button class="w-100 btn btn-lg btn-info mt-4" @click="login">Iniciar Sesión</button>
                         <p class="mt-5 mb-3 text-muted">
                             <a id="forgot-password" href="#">¿Ha olvidado su contraseña?</a>
                         </p>
@@ -40,19 +39,19 @@ export default defineComponent({
         const authService = new AuthService()
 
         const userLogin = ref<userLogin>({
-            userName: '',
+            rut: '',
             password: ''
         });
 
         const verifyForm = () => {
-            if (userLogin.value.userName.length === 0) return 1
+            if (userLogin.value.rut.length === 0) return 1
             if (userLogin.value.password.length === 0) return 2
             return 0
         };
 
         const login = async () => {
             const verification = verifyForm();
-            if (verification === 1 || verification === 2) console.log(verification === 1 ? 'debe ingresar un nombre de usuario' : 'debe ingresar una contraseña')
+            if (verification === 1 || verification === 2) console.log(verification === 1 ? 'debe ingresar un rut' : 'debe ingresar una contraseña')
             if (verification === 0) {
                 await authService.login(userLogin.value)
                     .then((response) => {
@@ -61,15 +60,12 @@ export default defineComponent({
                             authStore.setUser(user)
                             authStore.setToken(token)
                             authService.hasValidUserSession()
-                            router.push('home')
+                            router.push('profesionals')
                         } else {
                             console.log('no se pudo Logear');
                         }
                     })
-                router.push('home')
-
             }
-
         };
 
         return { userLogin, login };
@@ -87,13 +83,15 @@ export default defineComponent({
 }
 
 .vertical-center {
+    color: rgb(27, 90, 141);
+    background : rgb(88, 240, 194);
     display: flex;
     justify-content: center;
     align-items: center;
 }
 
 .logo-container {
-    background-color: #ffd867;
+    background-color: #ffffff;
 }
 
 .logo-container img {
