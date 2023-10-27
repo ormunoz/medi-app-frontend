f8220bc26bdf81d9a2d851ba80a9ef68aefb6d4a
ormunoz <or.munozf@gmail.com>
2023-10-27 18:02:50 -0300

    se agregan nuevas clases para usuario

--- src/service/users/types.ts
+++ src/service/users/types.ts
@@ -1,22 +1,94 @@
-// import { User } from "../auth/types";
+export class Patient {
+    email: string;
+    id: number;
+    lastName: string;
+    city: number;
+    totalScore: number;
+    name: string;
+    assignedProfessional: number;
+    user: {
+        id: number;
+        rut: string;
+        password: string;
+        role: string;
+    };
+    profesional: {
+        id: number;
+        name: string;
+        lastName: string;
+        especiality: string;
+    };
+    userId: number;
+    data: any;
 
-export interface Profesional {
-    id: number,
-    rut: string,
-    role: string,
+    constructor(data: any) {
+        this.email = data.email || '';
+        this.id = data.id || 0;
+        this.lastName = data.lastName || '';
+        this.city = data.city || '';
+        this.totalScore = data.totalScore || 0;
+        this.assignedProfessional = data.assignedProfessional || 0;
+        this.name = data.name || '';
+        this.user = data.user || {
+            id: 0,
+            rut: '',
+            password: '',
+            role: '',
+        };
+        this.profesional = data.profesional || {
+            id: 0,
+            name: '',
+            lastName: '',
+            especiality: '',
+        };
+        this.userId = data.userId || 0;
+    }
+}
+
+export class Profesional {
+    especiality: string;
+    id: number;
+    lastName: string;
+    maxScore: number;
+    minScore: number;
+    name: string;
+    user: {
+        id: number;
+        rut: string;
+        password: string;
+        role: string;
+    };
+    userId: number;
+    data: any;
+
+    constructor(data: any) {
+        this.especiality = data.especiality || '';
+        this.id = data.id || 0;
+        this.lastName = data.lastName || '';
+        this.maxScore = data.maxScore || 0;
+        this.minScore = data.minScore || 0;
+        this.name = data.name || '';
+        this.user = data.user || {
+            id: 0,
+            rut: '',
+            password: '',
+            role: '',
+        };
+        this.userId = data.userId || 0;
+    }
 }
 
 
-// class UserInfo implements User {
-//     constructor(
-//         public id: number | undefined = undefined,
-//         public firstName: string | undefined = undefined,
-//         public lastName: string | undefined = undefined,
-//         public phone: string | undefined = undefined,
-//         public email: string | undefined = undefined,
-//         public dni: string | undefined = undefined
-//     ) { }
-// }
-
-
+export interface ProfesionalAdd {
+    [x: string]: any;
+    id: number;
+    role: string;
+    password: string;
+    rut: string;
+    especiality: string;
+    name: string;
+    lastName: string;
+    minScore: number;
+    maxScore: number;
+}
 
