5053451598c3b1147c0ab4259774a7cb17503537
ormunoz <or.munozf@gmail.com>
2023-10-28 14:45:25 -0300

    se cambia id de profesional por de user

--- src/views/menu/ProfesionalView.vue
+++ src/views/menu/ProfesionalView.vue
@@ -86,7 +86,7 @@ export default {
             });
 
             if (result.isConfirmed) {
-                const response = await userService.deleteProfesional(profesional.id);
+                const response = await userService.deleteProfesional(profesional.user.id);
                 toast.success("Profesional eliminado con exito", {
                     autoClose: 4000,
                 });
